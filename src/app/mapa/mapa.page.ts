4b0774f4d32cc169cd196ad4fc80fe662a8521dc
renatoo_95@live.com <renatoo_95@live.com>
2020-03-02 22:45:49 -0300

    bug com geolozalização em mapa

--- src/app/mapa/mapa.page.ts
+++ src/app/mapa/mapa.page.ts
@@ -1,37 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { Router } from '@angular/router';
-import { Map, tileLayer, marker, icon } from 'leaflet';
+import L, { Map, tileLayer, marker, icon, latLng } from 'leaflet';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { logging } from 'protractor';
 
 @Component({
   selector: 'app-mapa',
   templateUrl: 'mapa.page.html',
-  styleUrls: ['mapa.page.scss']
+  styleUrls: ['mapa.page.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MapaPage {
 
   constructor(
     public plt: Platform,
-    public router: Router) {}
+    public router: Router,
+    private geolocation: Geolocation,
+    private cd: ChangeDetectorRef) {}
 
     ngAfterViewInit() {
       this.initMap();
     }
-
+    
     initMap() {
-      const map = new Map('map').setView([33.6396965, -84.4304574], 23);
-  
-      tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      }).addTo(map);
-  
-      const customMarkerIcon = icon({
-        iconUrl: 'assets/images/custom-marker-icon.png',
-        iconSize: [64, 64], 
-        popupAnchor: [0, -20]
-      });
+      var latlng = L.latLng(-15.794230,-47.882166);
+
+      const map = new Map('map').setView(latlng, 10);
+    
+        tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+    
+        const customMarkerIcon = icon({
+          iconUrl: 'assets/images/custom-marker-icon.png',
+          iconSize: [64, 64], 
+          popupAnchor: [0, -20]
+        });
+        
+        this.geolocation.getCurrentPosition().then((position) =>{
+          latlng = L.latLng(position.coords.latitude,position.coords.longitude);
+          this.cd.detectChanges();
+        }, (err) => {        
+          console.log(err);
+        });
+      map.flyTo(latlng, 23);
     }
-
-
 
 }
